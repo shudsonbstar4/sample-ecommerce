15e731f4d8e9415bbd4e9031310fc5d55b833a95
Sarah Hudson <sarah@checkappointments.com>
2015-02-17 23:11:19 -0500

    Initial commit w/ skeleton and basic JS functions using sessionStorage

---
+++ cookie.js
@@ -0,0 +1,35 @@
+function writeCookie(name, value, days){
+	var expires = '';
+
+	if(days){
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toGMTString();
+	};
+
+	console.log("VALUE " + value);
+	if(cookieArray === undefined){
+		var cookieArray = [];
+	};
+
+	cookieArray.push(value);
+	console.log("COOKIE ARRAY " + JSON.stringify(cookieArray, true, 2));
+  	//document.cookie = name + "=" + value + expires + "; path=/";
+};
+
+function readCookie(name){
+	var searchName = name + "=";
+	var cookies = document.cookie.split(';'); //split returns an array
+	for(var i=0; i < cookies.length; i++){
+		var c = cookies[i];
+		while(c.charAt(0) == ' ')
+			c = c.substring(1, c.length);
+		if(c.indexOf(searchName) == 0)
+			return c.substring(searchName.length, c.length);
+	}
+	return null;
+};
+
+function eraseCookie(name){
+	writeCookie(name, "", -1);
+}
